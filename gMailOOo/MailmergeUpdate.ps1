611a462f48738199dc3be73c427002452508e08e
prrvchr <prrvchr@users.noreply.github.com>
2017-05-30 01:32:42 +0200

    Add files via upload

--- gMailOOo/MailmergeUpdate.ps1
+++ gMailOOo/MailmergeUpdate.ps1
@@ -3,5 +3,5 @@ Param(
     [string]$source,
     [string]$target
 )
-Start-Process -FilePath "$script" -Verb RunAs -ArgumentList "$source", "$target"
+Start-Process -FilePath "$script" -Verb RunAs -ArgumentList "`"$source`" `"$target`""
 Start-Sleep -Seconds 60
